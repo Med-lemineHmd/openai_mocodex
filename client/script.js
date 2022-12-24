ecc23732324c23a58d5d063c5792ee6968d89940
Med-lemineHmd <leminlemin00@gmail.com>
2022-12-24 22:01:39 +0100

    Start deploying

--- client/script.js
+++ client/script.js
@@ -10,10 +10,8 @@ function loader(element) {
   element.textContent = "";
 
   loadInterval = setInterval(() => {
-    // Update the text content of the loading indicator
     element.textContent += ".";
 
-    // If the loading indicator has reached three dots, reset it
     if (element.textContent === "....") {
       element.textContent = "";
     }
@@ -33,9 +31,6 @@ function typeText(element, text) {
   }, 20);
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -46,17 +41,14 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-        <div class="wrapper ${isAi && "ai"}">
-            <div class="chat">
-                <div class="profile">
-                    <img 
-                      src=${isAi ? bot : user} 
-                      alt="${isAi ? "bot" : "user"}" 
-                    />
-                </div>
-                <div class="message" id=${uniqueId}>${value}</div>
-            </div>
-        </div>
+    <div class="wrapper ${isAi && "ai"}">
+    <div class="chat">
+    <div class="profile">
+    <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
+    </div>
+    <div class="message" id=${uniqueId}>${value}</div>
+    </div>
+    </div>
     `;
 }
 
@@ -68,26 +60,22 @@ const handleSubmit = async (e) => {
   // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-  // to clear the textarea input
   form.reset();
 
   // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
-
-  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  // specific message div
   const messageDiv = document.getElementById(uniqueId);
-
-  // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codex-im0y.onrender.com/", {
+  // fetch data from server -> bot's response
+
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-type": "application/json",
     },
     body: JSON.stringify({
       prompt: data.get("prompt"),
@@ -95,17 +83,18 @@ const handleSubmit = async (e) => {
   });
 
   clearInterval(loadInterval);
-  messageDiv.innerHTML = " ";
+  messageDiv.innerHTML = "";
 
   if (response.ok) {
     const data = await response.json();
-    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+    const parsedData = data.bot.trim();
 
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
 
     messageDiv.innerHTML = "Something went wrong";
+
     alert(err);
   }
 };
